045a6d74a265845e9b75fc05ea457242e791d6ab
Quentin Tardivon <quentin.tardivon@telecomnancy.net>
2015-11-11 19:34:03 +0100

    Voilà, les  lignes du haut servent à importer les API, ensuite il s'agit juste d'un petit test pour voir si elles fonctionnent. (Atom annonce 3 erreurs, mais ce n'en sont pas)

--- main.scala
+++ main.scala
@@ -1,36 +1,21 @@
+#!/bin/sh
+exec scala -classpath "/home/quentin/Documents/TOP/Projet TOP/PROJET/2-API/imagewrapper_2.11-1.0.0.jar" "$0" "$@"
+!#
+
+
 object Main {
   def main(args: Array[String]) : Unit = {
 
-  :)
 
 
 
+    import com.tncy.top.image.ImageWrapper;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    var fileName : String = "sampleImage.jpg";
+    var wrappedImage : ImageWrapper = new ImageWrapper(fileName);
+    var image2D : Array[Array[Int]] = wrappedImage.getImage();
+    println("The image height is: " + wrappedImage.height + " px.");
+    println("The image width is: " + wrappedImage.width + " px.");
 
   }
 }
